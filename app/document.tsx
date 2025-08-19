e2c5e5b326817346826d367e4d60814f2f15e475
jkolsun <kolsunjc@gmail.com>
2025-08-19 16:00:47 -0400

    Added favicon to browser

---
+++ app/document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Brighton Road Landscaping - Affordable Lawn Care, Reliable Customer Service, Flexible Scheduling, Experienced Team Members." />
+        <meta name="keywords" content="Brighton Road Landscaping, Lawn Care, Landscaping Services, Montgomery County PA" />
+        <meta name="author" content="Brighton Road Landscaping" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
